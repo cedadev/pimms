d33ee29c6609555e421fb79e36577a6f6c2360bd
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-02-27 14:10:00 +0000

    Added a fabric task to generate the wsgi deployment script.
    This can replace the hard-wired apache/django.wsgi.  e.g.
    {{{
    $ fab make_wsgi:deployment=/path/to/deployment/virtualenv
    }}}
    will create ./apache/pimms.wsgi

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,10 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
+import os.path as op
+from string import Template
+
+here = op.dirname(__file__)
 
 env.hosts = ['gdevine@puma.nerc.ac.uk']
 
@@ -23,3 +27,31 @@ def deploy():
         run("ls -la")
         run("git pull")
         #run("touch app.wsgi")
+
+
+
+def make_wsgi(deployment=None, filepath=None):
+    """
+    Create the wsgi script used for deployment.
+
+    :param deployment: A path to the virtualenv used to deploy.  
+        This will default to the current virtualenv.  Note this can be set to
+        a path on another system where the code will be deployed.
+    :param filepath: The path to write the resulting wsgi file to.
+        This defaults to ./apache/pimms.wsgi
+
+    """
+
+    if deployment is None:
+        deployment = sys.prefix
+
+    if filepath is None:
+        filepath = op.join(here, 'apache/pimms.wsgi')
+
+    wsgi_template = op.join(here, 'apache/pimms.wsgi.tmpl')
+    t = Template(open(wsgi_template).read())
+    wsgi_src = t.substitute(DEPLOYMENT=deployment)
+
+    with open(filepath, 'w') as fh:
+        fh.write(wsgi_src)
+
