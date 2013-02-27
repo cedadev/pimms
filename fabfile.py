0d7b0bb9fb30f79c9e0486354b738b1412675f58
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-02-27 14:37:49 +0000

    Renamed make_wsgi and added more deployment build functionality.

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
+import sys
 import os.path as op
 from string import Template
 
@@ -9,6 +10,10 @@ here = op.dirname(__file__)
 
 env.hosts = ['gdevine@puma.nerc.ac.uk']
 
+# Defaults overridden in some tasks:
+env.deployment = sys.prefix
+env.apache_logdir = NotImplemented
+env.wsgi_path = NotImplemented
 
 def gitupdate():
     local("git add .")
@@ -29,29 +34,61 @@ def deploy():
         #run("touch app.wsgi")
 
 
+def ceda_deployment(deployment='/pyEnv/ve/pimms'):
+    """
+    Specific deployment configuration for CEDA.
 
-def make_wsgi(deployment=None, filepath=None):
+    """
+    wsgi_path = deployment+'/lib64/python2.6/site-packages/pimms/resources'
+    with settings(deployment=deployment,
+                  apache_logdir='/pyEnv/log',
+                  wsgi_path=wsgi_path):
+        wsgi()
+        wsgi_conf()
+    
+        
+
+def wsgi(filepath=None):
     """
     Create the wsgi script used for deployment.
 
-    :param deployment: A path to the virtualenv used to deploy.  
-        This will default to the current virtualenv.  Note this can be set to
-        a path on another system where the code will be deployed.
     :param filepath: The path to write the resulting wsgi file to.
         This defaults to ./apache/pimms.wsgi
 
     """
 
-    if deployment is None:
-        deployment = sys.prefix
 
     if filepath is None:
         filepath = op.join(here, 'apache/pimms.wsgi')
 
+
     wsgi_template = op.join(here, 'apache/pimms.wsgi.tmpl')
-    t = Template(open(wsgi_template).read())
-    wsgi_src = t.substitute(DEPLOYMENT=deployment)
+    write_template(wsgi_template, filepath,
+                   DEPLOYMENT=env.deployment)
 
-    with open(filepath, 'w') as fh:
+
+def wsgi_conf(filepath=None):
+    """
+    Create the apache configuration file for a wsgi deployment of PIMMS
+
+    :filepath: The output file to write the configuration file
+
+    """
+    if filepath is None:
+        filepath = op.join(here, 'apache/pimms_wsgi.conf')
+
+    params = {
+        'DEPLOYMENT': env.deployment,
+        'LOG_DIR': env.apache_logdir,
+        'PROJECT_WSGI': env.wsgi_path,
+        }
+    conf_template = op.join(here, 'apache/pimms_wsgi.conf.tmpl')
+    write_template(conf_template, filepath, **params)
+
+
+def write_template(template_file, output_file, **kwargs):
+    t = Template(open(template_file).read())
+    wsgi_src = t.substitute(kwargs)
+
+    with open(output_file, 'w') as fh:
         fh.write(wsgi_src)
-
