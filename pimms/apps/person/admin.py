20c8efefee4d173fc210a001ad17cc7a4ee780e5
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-03-07 13:23:52 +0000

    Made some relative imports absolute

--- pimms/apps/person/admin.py
+++ pimms/apps/person/admin.py
@@ -4,7 +4,7 @@ Created on 26 Sep 2012
 @author: gerarddevine
 '''
 from django.contrib import admin
-from apps.person.models import Person
+from pimms.apps.person.models import Person
 
 admin.site.register(Person)
 
