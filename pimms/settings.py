90f21cffead002e8a5bec8d28fa49effbcb79fb1
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-03-07 13:29:49 +0000

    Trivial template directory bug

--- pimms/settings.py
+++ pimms/settings.py
@@ -86,7 +86,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(thisDir, "templates")
+    os.path.join(thisDir, "templates"),
 )
 
 STATIC_DOC_ROOT = thisDir+'/media/'
